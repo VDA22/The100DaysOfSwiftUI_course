62967d702ca7c3bd6cced2d7bb91ce6b4044fc78
Viter <mrs_d_v@icloud.com>
2022-03-26 16:30:27 +0300

    update

--- Project_2_ GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project_2_ GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -18,30 +18,64 @@ struct ContentView: View {
 
 	@State private var correctAnswer = Int.random(in: 0 ... 2)
 
-    var body: some View {
+	var body: some View {
 		ZStack {
-			LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom 	)
+			RadialGradient(
+				stops: [
+					.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+					.init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+				],
+				center: .top,
+				startRadius: 200,
+				endRadius: 700
+			)
 				.ignoresSafeArea()
-			VStack(spacing: 30) {
-				VStack {
-					Text("Tap the flag of")
-						.font(.subheadline.weight(.heavy))
-					Text(countries[correctAnswer])
-						.font(.largeTitle.weight(.semibold))
-				}
-				.foregroundColor(.white)
 
-				ForEach(0 ..< 3) { number in
-					Button {
-						flagTapped(number)
-					} label: {
-						Image(countries[number].lowercased())
-							.renderingMode(.original)
-							.clipShape(Capsule())
-							.shadow(color: .white, radius: 5)
+			Spacer()
+
+			VStack {
+				Text("Guess the flag")
+					.font(.largeTitle.bold())
+					.foregroundColor(.white)
+
+				VStack(spacing: 15) {
+
+					VStack {
+						Text("Tap the flag of")
+							.font(.subheadline.weight(.heavy))
+							.foregroundStyle(.secondary)
+
+						Text(countries[correctAnswer])
+							.font(.largeTitle.weight(.semibold))
+					}
+
+					ForEach(0 ..< 3) { number in
+						Button {
+							flagTapped(number)
+						} label: {
+							Image(countries[number].lowercased())
+								.renderingMode(.original)
+								.clipShape(Capsule())
+								.shadow(color: .white, radius: 5)
+						}
 					}
 				}
+				.frame(maxWidth: .infinity)
+				.padding(.vertical, 20)
+				.background(.regularMaterial)
+				.clipShape(RoundedRectangle(cornerRadius: 20))
+
+				Spacer()
+				Spacer()
+
+				Text("Your score: ???")
+					.foregroundColor(.white)
+					.font(.title.bold())
+
+				Spacer()
 			}
+			.padding()
+
 		}
 		.alert(scoreTitle, isPresented: $showingScore) {
 			Button("Continue", action: askQuestion)
