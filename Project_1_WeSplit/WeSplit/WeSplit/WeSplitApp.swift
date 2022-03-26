f72d2ef260ec8390b2520085a434a3ad6c0e011f
Viter <mrs_d_v@icloud.com>
2022-03-26 14:16:37 +0300

    WeSplit_start

---
+++ Project_1_WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Darya Viter on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
