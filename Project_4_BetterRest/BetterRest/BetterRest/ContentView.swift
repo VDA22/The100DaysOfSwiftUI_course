3d6aad2970d6669818278681c9282e5fb2d2776e
Viter <mrs_d_v@icloud.com>
2022-05-03 21:13:03 +0300

    current logic

--- Project_4_BetterRest/BetterRest/BetterRest/ContentView.swift
+++ Project_4_BetterRest/BetterRest/BetterRest/ContentView.swift
@@ -5,31 +5,103 @@
 //  Created by Darya Viter on 25.04.2022.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
-	@State var agreedToTerms = false
-	@State var agreedToPrivacyPolicy = false
-	@State var agreedToEmails = false
+	@State private var wakeUp = defaultWakeTime
+	@State private var sleepAmount = 8.0
+	@State private var coffeeAmount = 1
+
+	@State private var alertTitle = ""
+	@State private var alertMessage = ""
+	@State private var showAlert = false
+
+	static var defaultWakeTime: Date {
+		var components = DateComponents()
+		components.hour = 7
+		components.minute = 0
+
+		return Calendar.current.date(from: components) ?? Date.now
+	}
 
 	var body: some View {
-		let agreedToAll = Binding(
-			get: {
-				agreedToTerms && agreedToPrivacyPolicy && agreedToEmails
-			},
-			set: {
-				agreedToTerms = $0
-				agreedToPrivacyPolicy = $0
-				agreedToEmails = $0
-			}
-		)
+		NavigationView {
+			Form {
+				VStack(alignment: .leading, spacing: 0) {
+					Text("When do you want to wake up?")
+						.font(.headline)
 
-		return VStack {
-			Toggle("Agree to terms", isOn: $agreedToTerms)
-			Toggle("Agree to privacy policy", isOn: $agreedToPrivacyPolicy)
-			Toggle("Agree to receive shipping emails", isOn: $agreedToEmails)
-			Toggle("Agree to all", isOn: agreedToAll)
+					DatePicker(
+						"Please, enter a time",
+						selection: $wakeUp,
+						displayedComponents: .hourAndMinute
+					)
+						.labelsHidden()
+
+				}
+
+				VStack(alignment: .leading, spacing: 0) {
+					Text("Desired amount of sleep")
+						.font(.headline)
+
+					Stepper(
+						"\(sleepAmount.formatted()) hours",
+						value: $sleepAmount,
+						in: 4 ... 12,
+						step: 0.25
+					)
+				}
+
+				VStack(alignment: .leading, spacing: 0) {
+					Text("Daily coffee intake")
+						.font(.headline)
+
+					Stepper(
+						coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+						value: $coffeeAmount,
+						in: 1 ... 20
+					)
+				}
+			}
+			.navigationTitle("Better rest")
+			.toolbar {
+				Button("Calculate", action: calculateBedTime)
+			}
+			.alert(alertTitle, isPresented: $showAlert) {
+				Button("OK") {}
+			} message: {
+				Text(alertMessage)
+			}
 		}
+	}
+
+	func calculateBedTime() {
+		do {
+			let config = MLModelConfiguration()
+			let model = try SleepCalculator(configuration: config)
+
+			let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+			let hour = (components.hour ?? 0) * 60 * 60
+			let minute = (components.minute ?? 0) * 60
+
+			let prediction = try model.prediction(
+				wake: Double(hour + minute),
+				estimatedSleep: sleepAmount,
+				coffee: Double(coffeeAmount)
+			)
+
+			let sleepTime = wakeUp - prediction.actualSleep
+
+			alertTitle = "Your ideal bedtime is..."
+			alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+
+		} catch {
+			alertTitle = "Error!"
+			alertMessage = "Sorry? there was a problem calculating your bedtime."
+		}
+
+		showAlert = true
 	}
 }
 
