a9e11b5e76548bcfeab87aae6f68aa481902a927
Viter <mrs_d_v@icloud.com>
2022-05-03 16:54:35 +0300

    start

---
+++ Project_4_BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Darya Viter on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project_4_BetterRest/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Darya Viter on 25.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	@State var agreedToTerms = false
+	@State var agreedToPrivacyPolicy = false
+	@State var agreedToEmails = false
+
+	var body: some View {
+		let agreedToAll = Binding(
+			get: {
+				agreedToTerms && agreedToPrivacyPolicy && agreedToEmails
+			},
+			set: {
+				agreedToTerms = $0
+				agreedToPrivacyPolicy = $0
+				agreedToEmails = $0
+			}
+		)
+
+		return VStack {
+			Toggle("Agree to terms", isOn: $agreedToTerms)
+			Toggle("Agree to privacy policy", isOn: $agreedToPrivacyPolicy)
+			Toggle("Agree to receive shipping emails", isOn: $agreedToEmails)
+			Toggle("Agree to all", isOn: agreedToAll)
+		}
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
