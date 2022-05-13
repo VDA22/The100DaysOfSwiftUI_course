397e91e5f58328896d2af3230700160fced46e56
Viter <mrs_d_v@icloud.com>
2022-05-13 14:53:22 +0300

    ContentView

--- Project_6_Animations/Animations/Animations/ContentView.swift
+++ Project_6_Animations/Animations/Animations/ContentView.swift
@@ -8,9 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                NavigationLink {
+                    ScaleButtonView()
+                } label: {
+                    Image(systemName: "eye.slash.circle")
+                        .font(.title)
+                    Text("Implicit animation")
+                }
+                
+                NavigationLink {
+                    TwistButtonView()
+                } label: {
+                    Image(systemName: "eye.circle")
+                        .font(.title)
+                    Text("Explicit animation")
+                }
+                
+                NavigationLink {
+                    ScaleStepperAndButtonView()
+                } label: {
+                    Image(systemName: "link.circle")
+                        .font(.title)
+                    Text("Animating bindings")
+                }
+                
+                NavigationLink {
+                    RoundCornersButton()
+                } label: {
+                    Image(systemName: "list.bullet.circle")
+                        .font(.title)
+                    Text("Controlling Animation")
+                }
+                
+                NavigationLink {
+                    DragCardView()
+                } label: {
+                    Image(systemName: "dot.circle.and.hand.point.up.left.fill")
+                        .font(.title)
+                    Text("Gesture animation")
+                }
+                
+                NavigationLink {
+                    TransitionRectangleView()
+                } label: {
+                    Image(systemName: "circle.dotted")
+                        .font(.title)
+                    Text("Transition animation")
+                }
+            }
+            .navigationTitle("Animations")
+            .navigationViewStyle(.stack)
+        }
     }
 }
 
