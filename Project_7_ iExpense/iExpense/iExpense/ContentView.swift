b4b961c6592c542257836948502fb98fe591b437
Darya Viter <cleverdevil@yandex.ru>
2022-05-30 13:03:52 +0300

    start

---
+++ Project_7_ iExpense/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Darya Viter on 30.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
