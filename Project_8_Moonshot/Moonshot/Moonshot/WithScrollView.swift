8530aa169f508dc15d4cdc700b6d6a4772526464
Darya Viter <cleverdevil@yandex.ru>
2022-06-02 14:03:17 +0300

    WithScrollView

---
+++ Project_8_Moonshot/Moonshot/Moonshot/WithScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  WithScrollView.swift
+//  Moonshot
+//
+//  Created by Darya Viter on 02.06.2022.
+//
+
+import SwiftUI
+
+struct WithScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WithScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        WithScrollView()
+    }
+}
