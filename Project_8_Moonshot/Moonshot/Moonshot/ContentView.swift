5a653d85e6c5e0af25e3f61e9dd3040aa8f4b642
Darya Viter <cleverdevil@yandex.ru>
2022-06-02 12:09:08 +0300

    start

---
+++ Project_8_Moonshot/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Darya Viter on 02.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
