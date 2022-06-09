553d83a0211c5686fe280d0b94927fd0e7eb6e65
Darya Viter <cleverdevil@yandex.ru>
2022-06-09 11:21:07 +0300

    Form with views

--- Project_9_Drawing/Drawing/Drawing/ContentView.swift
+++ Project_9_Drawing/Drawing/Drawing/ContentView.swift
@@ -9,8 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                NavigationLink {
+                    PathView()
+                } label: {
+                    Label("Path", systemImage: "arrow.rectanglepath")
+                }
+
+                NavigationLink {
+                    ShapesView()
+                } label: {
+                    Label("Shapes", systemImage: "square.fill.on.circle.fill")
+                }
+
+                NavigationLink {
+                    AnimateShapeView()
+                } label: {
+                    Label("Animate shapes", systemImage: "squareshape.squareshape.dashed")
+                }
+
+                NavigationLink {
+                    AnimateMultyDataView()
+                } label: {
+                    Label("Animate multy data shapes", systemImage: "squareshape.dashed.squareshape")
+                }
+
+                NavigationLink {
+                    BlendColorsView()
+                } label: {
+                    Label("Blend colors", systemImage: "circle.and.line.horizontal.fill")
+                }
+
+                NavigationLink {
+                    ImagePicturedView()
+                } label: {
+                    Label("Image as border", systemImage: "square.dashed")
+                }
+
+                NavigationLink {
+                    ColoringCirclesView()
+                } label: {
+                    Label("Coloring circles", systemImage: "circle.circle")
+                }
+
+                NavigationLink {
+                    FlowerView()
+                } label: {
+                    Label("Flower", systemImage: "circle.hexagonpath")
+                }
+
+                NavigationLink {
+                    SpirographView()
+                } label: {
+                    Label("Spirograph", systemImage: "wand.and.stars.inverse")
+                }
+            }
+            .navigationTitle("Work With Path")
+
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
