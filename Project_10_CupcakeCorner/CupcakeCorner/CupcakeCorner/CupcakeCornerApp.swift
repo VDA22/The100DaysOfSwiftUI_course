b80587124cdb8322b3aff4c3334c3d3e03357675
Darya Viter <cleverdevil@yandex.ru>
2022-06-12 12:12:17 +0300

    start

---
+++ Project_10_CupcakeCorner/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Darya Viter on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
