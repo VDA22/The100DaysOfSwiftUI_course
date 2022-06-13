3ed67a2c05b585a42c1564f7fa04e9a384aa73c1
Darya Viter <cleverdevil@yandex.ru>
2022-06-13 13:56:57 +0300

    start

---
+++ Project_11_Bookworm/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Darya Viter on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
