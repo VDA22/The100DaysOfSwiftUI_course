a94faa63318f112306eb07365cb6625a1e387ead
Darya Viter <cleverdevil@yandex.ru>
2022-06-16 18:32:27 +0300

    DetailView

---
+++ Project_11_Bookworm/Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,43 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Darya Viter on 16.06.2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+            Text(book.author ?? "Unknown Author")
+                .font(.title)
+                .foregroundColor(.secondary)
+
+            Text(book.review ?? "No Review")
+                .padding()
+
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "Unknown Book")
+        .navigationViewStyle(.stack)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
