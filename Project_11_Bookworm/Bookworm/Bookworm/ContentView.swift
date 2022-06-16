076b603a596beb85d5bb97645c70651fd182492c
Darya Viter <cleverdevil@yandex.ru>
2022-06-16 18:39:28 +0300

    add deleteBooks

--- Project_11_Bookworm/Bookworm/Bookworm/ContentView.swift
+++ Project_11_Bookworm/Bookworm/Bookworm/ContentView.swift
@@ -37,10 +37,15 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks(at:))
             }
             .navigationTitle("Bookworm")
             .navigationViewStyle(.stack)
             .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         showingAddScreen.toggle()
@@ -54,6 +59,15 @@ struct ContentView: View {
             }
 
         }
+    }
+
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+
+//        try? moc.save()
     }
 }
 
