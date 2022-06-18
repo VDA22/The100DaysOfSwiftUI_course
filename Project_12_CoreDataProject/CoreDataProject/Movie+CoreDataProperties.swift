9cd513457205b42e7cd24a7fe10d0d02b15c0d7c
Darya Viter <cleverdevil@yandex.ru>
2022-06-18 17:14:21 +0300

    Movie+CoreDataClass

---
+++ Project_12_CoreDataProject/CoreDataProject/Movie+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  Movie+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Darya Viter on 18.06.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Movie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+        return NSFetchRequest<Movie>(entityName: "Movie")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var director: String?
+    @NSManaged public var year: Int16
+
+    public var wrappedTitle: String {
+        title ?? "Unknown Title"
+    }
+}
+
+extension Movie : Identifiable {
+
+}
