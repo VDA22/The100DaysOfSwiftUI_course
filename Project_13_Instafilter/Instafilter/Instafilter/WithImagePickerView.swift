5966ae98ee54b43589eec09ad8b2c05441a249d4
Darya Viter <cleverdevil@yandex.ru>
2022-06-23 11:44:21 +0300

    WithImagePickerView + ImagePicker + Coordinator

---
+++ Project_13_Instafilter/Instafilter/Instafilter/WithImagePickerView.swift
@@ -0,0 +1,43 @@
+//
+//  WithImagePickerView.swift
+//  Instafilter
+//
+//  Created by Darya Viter on 23.06.2022.
+//
+
+import SwiftUI
+
+struct WithImagePickerView: View {
+    @State private var image: Image?
+    @State private var inputImage: UIImage?
+    @State private var showingImagePicker = false
+
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+
+            Button("Select image") {
+                showingImagePicker = true
+            }
+        }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker(image: $inputImage)
+        }
+        .onChange(of: inputImage) { _ in loadImage() }
+    }
+
+    private func loadImage() {
+        guard let inputImage = inputImage else {
+            return
+        }
+        image = Image(uiImage: inputImage)
+    }
+}
+
+struct WithImagePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        WithImagePickerView()
+    }
+}
