096383400c44e3bf892c82edab7414d5f5c31e84
Darya Viter <cleverdevil@yandex.ru>
2022-06-26 15:57:56 +0300

    WriteMessageView

---
+++ Project_14_BucketList/BucketList/BucketList/WriteMessageView.swift
@@ -0,0 +1,37 @@
+//
+//  WriteMessageView.swift
+//  BucketList
+//
+//  Created by Darya Viter on 26.06.2022.
+//
+
+import SwiftUI
+
+struct WriteMessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onTapGesture {
+                let str = "Test Message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
+
+struct WriteMessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        WriteMessageView()
+    }
+}
