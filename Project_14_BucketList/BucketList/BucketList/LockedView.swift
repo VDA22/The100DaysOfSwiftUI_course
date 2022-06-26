a31de5983b9a0b34ad4b9afd73d08e551a04179b
Darya Viter <cleverdevil@yandex.ru>
2022-06-26 17:21:41 +0300

    LockedView

---
+++ Project_14_BucketList/BucketList/BucketList/LockedView.swift
@@ -0,0 +1,51 @@
+//
+//  LockedView.swift
+//  BucketList
+//
+//  Created by Darya Viter on 26.06.2022.
+//
+
+import LocalAuthentication
+import SwiftUI
+
+struct LockedView: View {
+    @State private var isUnlock = false
+
+    var body: some View {
+        VStack {
+            if isUnlock {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+
+    private func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // reason for TouchID
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+
+                if success {
+                    isUnlock = true
+                } else {
+                    print(authenticationError?.localizedDescription)
+                }
+            }
+        } else {
+            print("No FaceID or TouchID")
+        }
+    }
+}
+
+struct LockedView_Previews: PreviewProvider {
+    static var previews: some View {
+        LockedView()
+    }
+}
