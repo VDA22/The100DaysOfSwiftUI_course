c1469876285b666164247a1afdbf36ee19776468
Darya Viter <cleverdevil@yandex.ru>
2022-06-26 15:35:19 +0300

    start

---
+++ Project_14_BucketList/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Darya Viter on 26.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
