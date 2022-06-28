5fccce3b802969b7cb3109a006533f67a384658e
Darya Viter <cleverdevil@yandex.ru>
2022-06-28 13:54:52 +0300

    start

---
+++ Project_16_HotProspects/HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Darya Viter on 28.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
