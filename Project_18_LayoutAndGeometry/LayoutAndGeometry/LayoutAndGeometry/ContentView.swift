f43d78f475b8ed3d3626d3266bd040b06a857a85
Darya Viter <cleverdevil@yandex.ru>
2022-07-04 15:35:55 +0300

    start

---
+++ Project_18_LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Darya Viter on 04.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project_18_LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Darya Viter on 04.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
